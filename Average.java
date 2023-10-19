a9abdf9b03720af4d2e5b2d515b6fbf112c43212
Tejas-Pulli <115936478+Tejas-Pulli@users.noreply.github.com>
2023-10-20 02:44:05 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -3,24 +3,9 @@ class Average
 {
  	public static void main(String[] args)
  	{ 
-  	Scanner  a = new Scanner(System.in);
-	int i=0;
-	int avg=0;
-	
+  	
   
-	System.out.println("Enter Number of Terms: "); 
-    	int b = a.nextInt();
-
-	System.out.println("Enter Numbers: ");  
-	while(i<b)
-	{
-    		
-    		avg = avg+a.nextInt();
-		i++;
-	}
-		avg=avg/b;
-	System.out.printf("Average = %d ",avg);  
-
+	System.out.println("Hello World"); 
 	}
 
 }
